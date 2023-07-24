aab8c458940c3427b34949c4e7bb38ebf3662df2
Aiswarya R <aiswarya.r@experionglobal.com>
2023-07-24 09:24:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header className="App-header">  
+      <p>Welcome to a sample react app!</p>
+   <img src="https://www.ffwd.org/wp-content/uploads/Hello-World-logo.png"/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
